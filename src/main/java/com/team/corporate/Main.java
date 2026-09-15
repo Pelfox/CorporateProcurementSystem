27b1c08800b19c9332c6799671b818f29a5f930c
critical_error <->
2026-09-15 22:41:31 +0300

    feat(domain): configure Hibernate to connect to the database.

--- src/main/java/com/team/corporate/Main.java
+++ src/main/java/com/team/corporate/Main.java
@@ -3,9 +3,7 @@ package com.team.corporate;
 import com.team.corporate.interactions.ConsoleInteraction;
 import com.team.corporate.interactions.InteractionAction;
 import com.team.corporate.interactions.InteractionInterface;
-import com.team.corporate.util.*;
 
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.function.Consumer;
 
@@ -22,12 +20,6 @@ public class Main {
         while (nextAction != null) {
             System.out.println(nextAction);
             nextAction = interaction.getNextAction();
-        }
-
-        try (var connection = DatabaseManager.openConnection()) {
-            IO.println(connection.getTransactionIsolation());
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
         }
     }
 }
